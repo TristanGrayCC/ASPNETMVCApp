b44189987db88b8cfe566020e7c12625da9e95ad
tristangraycc <tristangraycc@gmail.com>
2018-11-12 22:48:52 +0000

    Database set up

--- ASPNETMVCApp/Program.cs
+++ ASPNETMVCApp/Program.cs
@@ -1,17 +1,40 @@
-﻿namespace ASPNETMVCApp
-{
-    using Microsoft.AspNetCore;
-    using Microsoft.AspNetCore.Hosting;
+﻿using ASPNETMVCApp;
+using ASPNETMVCApp.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
+namespace BethanysPieShop
+{
     public class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>();
+                    DbInitialiser.Seed(context);
+                }
+                catch (Exception)
+                {
+                    //we could log this in a real-world situation
+                }
+            }
+
+            host.Run();
+
+            //BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>().Build();
+                .UseStartup<Startup>()
+                .Build();
     }
 }
